4e59e1669763fc8212cbf4fe032c760eba956378
Chroma7p <nahco3.hcl@gmail.com>
2022-08-28 03:05:02 +0900

    コメント追加

--- app/util/dict_cog.py
+++ app/util/dict_cog.py
@@ -3,15 +3,18 @@ import subprocess
 
 DICT_PATH="../dict.txt"
 
+
 async def addDict(arg1,arg2):
+    #辞書ファイルの末尾に追記
     with open(DICT_PATH, mode='a') as f:
         f.write(arg1 + ',' + arg2+'\n')
 
+
 def replaceDict(text):
+    #辞書に基づいて置換
     f = open(DICT_PATH, 'r')
     lines = f.readlines()
     print(lines)
-
     for line in lines:
         pattern = line.strip().split(',')
         if pattern[0] in text and len(pattern) >= 2:
@@ -19,7 +22,9 @@ def replaceDict(text):
     f.close()
     return text
 
+
 def showDict():
+    #辞書を読み込んで表示
     f = open(DICT_PATH, 'r')
     lines = f.readlines()
     output = "現在登録されている辞書一覧\n"
@@ -30,6 +35,7 @@ def showDict():
     return output
 
 async def removeDict(num):
+    #辞書の削除
     try:
         cmd = ["sed", "-i.bak","-e", ("{0}d").format(num),"dict.txt"]
         subprocess.call(cmd)
@@ -39,25 +45,19 @@ async def removeDict(num):
     return 1
 
 
-async def removeDict(num):
-    try:
-        cmd = ["sed", "-i.bak", "-e", ("{0}d").format(num), "dict.txt"]
-        subprocess.call(cmd)
-    except Exception as e:
-        print(e)
-        return 0
-    return 1
-
 class DictCog(commands.Cog):
+    #辞書管理用のコマンドを集めたコグ
     def __init__(self,bot):
         self.bot=bot
 
     @commands.command()
     async def get(self,ctx):
+        #辞書を表示(!get)
         await ctx.channel.send(showDict())
     
     @commands.command()
     async def add(self,ctx,a,b):
+        #辞書に追加(!add xxx yyy)
         if len(a) > 10 or len(b) > 10:
             await ctx.channel.send("荒らしは許されませんよ♡")
             return
@@ -66,6 +66,7 @@ class DictCog(commands.Cog):
 
     @commands.command()
     async def remove(self,ctx,num):
+        #辞書を削除(!remove 3)
         if await removeDict(num):
             await ctx.channel.send("削除しました")
         else:
--- app/util/yomiage_setting_cog.py
+++ app/util/yomiage_setting_cog.py
@@ -11,38 +11,39 @@ class YomiageSettingCog(commands.Cog):
 
     @commands.command()
     async def join(self,ctx):
-        #VCにログイン
+        #VCにログイン(!join)
         self.currentChannel = ctx.channel
         self.voice = await ctx.channel.connect()
         await ctx.channel.send('ボイスチャンネルにログインしました')
 
     @commands.command()
     async def dc(self,ctx):
-        #VCからログアウト
+        #VCからログアウト(!dc)
         await self.voice.disconnect()
         self.currentChannel = None
         await ctx.channel.send('ボイスチャンネルからログアウトしました')
 
     @commands.command()
     async def status(self,ctx):
-        #状態の表示
+        #状態の表示(!status)
         if self.voice.is_connected():
             await ctx.channel.send('ボイスチャンネルに接続中です')
 
     @commands.command()
     async def vol(self,ctx,arg=""):
-        #音量調整
-        send=""
+        #音量調整(!vol up(音量上げ),!vol down(音量下げ),!vol(音量確認))
         if arg=="up":
             self.volume+=0.1
-            send+="音量を上げました\n"
+            send="音量を上げました\n"
         elif arg=="down":
             self.volume-=0.1
-            send+="音量を下げました\n"
+            send="音量を下げました\n"
+        else:
+            send=""
         await ctx.channel.send(send+f"現在の音量は{self.volume}です")
 
 
 def setup(bot):
     #Cogとして使うのに必要なsetup関数
-    print("YomiageCog OK")
+    print("YomiageSettingCog OK")
     return bot.add_cog(YomiageSettingCog(bot))
