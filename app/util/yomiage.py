2d6188663de920b2e0e59062e6195dd6514896d6
Chroma7p <nahco3.hcl@gmail.com>
2022-08-28 02:53:37 +0900

    コメントを追加、関数の順番を変更

--- app/util/yomiage.py
+++ app/util/yomiage.py
@@ -1,9 +1,7 @@
 import os
 import re
 import time
-import json
 import discord
-
 import subprocess
 from threading import Timer
 from dict_cog import DICT_PATH
@@ -14,7 +12,9 @@ url = re.compile('^http')
 mention = re.compile('<@[^>]*>*')
 queue_dict = defaultdict(deque)
 
+
 def replaceDict(text):
+    #テキストを置換
     f = open(DICT_PATH, 'r')
     lines = f.readlines()
     print(lines)
@@ -26,7 +26,9 @@ def replaceDict(text):
     f.close()
     return text
 
+
 async def replaceUserName(text):
+    #ユーザー名を置換
     for word in text.split():
         if not mention.match(word):
             continue
@@ -40,46 +42,9 @@ async def replaceUserName(text):
         text = text.replace(word, '@'+userName)
     return text
 
-def play(voice_client, queue):
-    if not queue or voice_client.is_playing():
-        return
-    source = queue.popleft()
-    # os.remove(source[1])
-    voice_client.play(source[0], after=lambda e: play(voice_client, queue))
-
-
-def current_milli_time():
-    return round(time.time() * 1000)
-
-class CommonModule:
-    def load_json(self, file):
-        with open(file, 'r', encoding='utf-8') as f:
-            json_data = json.load(f)
-        return json_data
-
-
-async def jtalk(t):
-    open_jtalk = ['open_jtalk']
-    mech = ['-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic']
-    htsvoice = ['-m', '/usr/share/hts-voice/mei/mei_normal.htsvoice']
-    pitch = ['-fm', '-5']
-    speed = ['-r', '1.0']
-    file = str(current_milli_time())
-    outwav = ['-ow', file + '.wav']
-    cmd = open_jtalk+mech+htsvoice+pitch+speed+outwav
-    c = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-    c.stdin.write(t.encode())
-    c.stdin.close()
-    c.wait()
-    return file + '.wav'
-
-def enqueue(voice_client, guild, source,filename):
-    queue = queue_dict[guild.id]
-    queue.append([source,filename])
-    if not voice_client.is_playing():
-        play(voice_client, queue)
 
 async def read_message(message,currentChannel):
+    #受け取ったメッセージを読み上げる
     text = message.content
     print( message.channel,currentChannel)
     if message.guild.voice_client:
@@ -96,6 +61,7 @@ async def read_message(message,currentChannel):
 
 
 async def play_process(text,send_to,voice_client):
+    #音声を合成して、キューに突っ込む
     filename = await jtalk(text)
     if os.path.getsize(filename) > 10000000:
             await send_to.send("再生時間が長すぎるよ")
@@ -104,3 +70,37 @@ async def play_process(text,send_to,voice_client):
                 discord.FFmpegPCMAudio(filename),filename)
     timer = Timer(3, os.remove, (filename, ))
     timer.start()
+
+
+async def jtalk(t):
+    #subprocessでコマンド叩いて音声合成してファイル名を返す
+    open_jtalk = ['open_jtalk']
+    mech = ['-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic']
+    htsvoice = ['-m', '/usr/share/hts-voice/mei/mei_normal.htsvoice']
+    pitch = ['-fm', '-5']
+    speed = ['-r', '1.0']
+    file = str(round(time.time() * 1000))
+    outwav = ['-ow', file + '.wav']
+    cmd = open_jtalk+mech+htsvoice+pitch+speed+outwav
+    c = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+    c.stdin.write(t.encode())
+    c.stdin.close()
+    c.wait()
+    return file + '.wav'
+
+
+def enqueue(voice_client, guild, source,filename):
+    #キューに突っ込む
+    queue = queue_dict[guild.id]
+    queue.append([source,filename])
+    if not voice_client.is_playing():
+        play(voice_client, queue)
+
+
+def play(voice_client, queue):
+    #キューを一個ずつ再生
+    if not queue or voice_client.is_playing():
+        return
+    source = queue.popleft()
+    # os.remove(source[1])
+    voice_client.play(source[0], after=lambda e: play(voice_client, queue))
